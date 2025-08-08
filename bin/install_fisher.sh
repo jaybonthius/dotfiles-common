e0db406a8921fd189eee69ab8fb382bd121c2583
Jay Bonthius <jaybonthius@gmail.com>
2025-08-07 23:50:14 -0700

    install fisher

---
+++ bin/install_fisher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install Fisher package manager for Fish shell
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
